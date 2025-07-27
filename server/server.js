7d84138539f726d213af6d525b43b10c40a08ab6
Scriptzstarling <shashwat11muz@gmail.com>
2025-07-27 20:32:29 +0530

    backend setup done started frontend

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js"
+import userRouter from "./routes/userRoutes.js";
 
 
 const app = express();
@@ -17,6 +18,8 @@ app.use(cors({credentials: true}));
 //API END POINT
 app.get('/', (req, res) => res.send("API WORKING"));
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
+
 app.listen(port, ()=> console.log(`Server started on PORT:${port}`));
 
 
